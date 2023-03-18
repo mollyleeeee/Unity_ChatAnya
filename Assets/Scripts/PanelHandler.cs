4b40a65b4c7204959050ce1a0d2814efac9f6f79
mollyleeeee <240638573@qq.com>
2023-03-18 18:27:52 +0800

    迭代

--- Assets/Scripts/PanelHandler.cs
+++ Assets/Scripts/PanelHandler.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using GPTClient;
 using System.Threading.Tasks;
 using Crosstales.RTVoice.Tool;
 using Crosstales.RTVoice;
+using ChatGPTExampleName;
 
 public class PanelHandler : MonoBehaviour {
 	TMP_InputField inputContent;
 	Button btn;
 	TMP_Text resText;
 	Animator girl;
-	GPTUtil gptUtil = new GPTUtil();
 	string rule = "Anya";
 	// 动画关键词
 	string[] keywords = {"a", "e", "i", "o", "u","早上好","晚安"};
@@ -44,36 +43,41 @@ public class PanelHandler : MonoBehaviour {
 		requestGPTSync(inputContent.text);
 	}
 
-	private void playAnimation(string state, float duration) {
-		state = keywords[animationIndex];
+	// 播放动画状态
+	private void playAnimation(float duration) {
+		var state = keywords[animationIndex];
 		animationIndex = (animationIndex + 1) %keywords.Length;
 		girl.CrossFade(state, duration);
 	}
 
 	private async void requestGPTSync(string prompt) {
-		 // 异步调用 request 方法并等待其返回结果
-    	string result = await SocketClient.request(prompt);
+    	// string result = await SocketClient.request(prompt);
+		// 请求 GPT API，需要配置Token
+		string result = await ChatGPTExample.chat(prompt);
 		string content = format(result);
 		// 在UI线程中更新文本控件的内容
     	updateResponseText(conbineRule(content));
-		// 播放名为"morning"的动画状态，并在0.02秒内平滑地将当前动画状态过渡到新的动画状态
-		playAnimation("morning", 0.02f);
+		// 播放动画状态，并在0.02秒内平滑地将当前动画状态过渡到新的动画状态
+		playAnimation(0.02f);
 		speak(content);
 	}
 
+	// 朗读
 	private void speak(string content) {
 		Speaker.Speak(content, null, null, true, 1, 1, "", 1);
 	}
 
+	// format 返回语句格式
 	private string format(string prompt) {
 		return prompt.TrimStart('\n');
 	}
 
+	// 角色名+回答
 	private string conbineRule(string prompt) {
 		return rule +":"+"\n"+prompt;
 	}
 
-
+	// 回复面板
 	private void updateResponseText(string response) {
 		resText.SetText(response);
 	}
